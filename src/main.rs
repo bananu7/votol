da0b65c33ec48d1a28e661d23ac7e33f4f5956e3
bananu7 <bananu7@o2.pl>
2025-02-01 20:25:56 +0100

    Add rust implementation that simply polls the controller on the serial port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,83 @@
+use std::io::{self, Write};
+use std::time::Duration;
+use std::{num::ParseIntError};
+
+use clap::{Arg, Command};
+
+pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
+    (0..s.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("Serialport Example - Receive Data")
+        .about("Reads data from a serial port and echoes it to stdout")
+        .disable_version_flag(true)
+        .arg(
+            Arg::new("port")
+                .help("The device path to a serial port")
+                .use_value_delimiter(false)
+                .required(true),
+        )
+        .arg(
+            Arg::new("baud")
+                .help("The baud rate to connect at")
+                .use_value_delimiter(false)
+                .required(true)
+                .validator(valid_baud),
+        )
+        .get_matches();
+
+    let port_name = matches.value_of("port").unwrap();
+    let baud_rate = matches.value_of("baud").unwrap().parse::<u32>().unwrap();
+
+    let port = serialport::new(port_name, baud_rate)
+        .timeout(Duration::from_millis(10))
+        .open();
+
+    // prepare votol mesage
+    let votol_str = "c9140253484f570000000000aa00000018aa00000000c40d";
+    let votol_question = decode_hex(votol_str).unwrap();
+
+    match port {
+        Ok(mut port) => {
+            loop {
+                match port.write(&votol_question) {
+                    Ok(_) => {
+                        std::io::stdout().flush().unwrap();
+                    }
+                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+                    Err(e) => eprintln!("{:?}", e),
+                }
+
+                let mut serial_buf: Vec<u8> = vec![0; 1000];
+                println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
+
+                match port.read(serial_buf.as_mut_slice()) {
+                    Ok(t) => {
+                        //io::stdout().write_all(&serial_buf[..t]).unwrap();
+                        println!("{:#04X?}", &serial_buf[..t]);
+
+                        io::stdout().flush().unwrap();
+                    }
+                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+                    Err(e) => eprintln!("{:?}", e),
+                }
+
+                std::thread::sleep(Duration::from_secs(1));
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
+            ::std::process::exit(1);
+        }
+    }
+}
+
+fn valid_baud(val: &str) -> Result<(), String> {
+    val.parse::<u32>()
+        .map(|_| ())
+        .map_err(|_| format!("Invalid baud rate '{}' specified", val))
 }
