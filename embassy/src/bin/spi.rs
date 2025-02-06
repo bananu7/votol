dd96b940341ad587fce544d7d37a20ba4ce9c35c
bananu7 <bananu7@o2.pl>
2025-02-07 00:52:10 +0100

    Added digits

--- embassy/src/bin/spi.rs
+++ embassy/src/bin/spi.rs
@@ -10,6 +10,123 @@ use {defmt_rtt as _, panic_probe as _};
 use embassy_stm32::spi::{Config, Spi};
 use embassy_stm32::time::Hertz;
 
+/* max7219 digit helpers */
+fn output_digit(digit: u8) -> [u8; 8] {
+    let to_write = match digit {
+        0 => [
+            0b11100000,
+            0b10100000,
+            0b10100000,
+            0b10100000,
+            0b10100000,
+            0b11100000,
+            0b00000000,
+            0b00000000,
+        ],
+        1 => [
+            0b01000000,
+            0b11000000,
+            0b01000000,
+            0b01000000,
+            0b01000000,
+            0b11100000,
+            0b00000000,
+            0b00000000,
+        ],
+        2 => [
+            0b11100000,
+            0b00100000,
+            0b11100000,
+            0b10000000,
+            0b10000000,
+            0b11100000,
+            0b00000000,
+            0b00000000,
+        ],
+        3 => [
+            0b11100000,
+            0b00100000,
+            0b11100000,
+            0b00100000,
+            0b00100000,
+            0b11100000,
+            0b00000000,
+            0b00000000,
+        ],
+        4 => [
+            0b10100000,
+            0b10100000,
+            0b11100000,
+            0b00100000,
+            0b00100000,
+            0b00100000,
+            0b00000000,
+            0b00000000,
+        ],
+        5 => [
+            0b11100000,
+            0b10000000,
+            0b11100000,
+            0b00100000,
+            0b00100000,
+            0b11100000,
+            0b00000000,
+            0b00000000,
+        ],
+        6 => [
+            0b11100000,
+            0b10000000,
+            0b11100000,
+            0b10100000,
+            0b10100000,
+            0b11100000,
+            0b00000000,
+            0b00000000,
+        ],
+        7 => [
+            0b11100000,
+            0b00100000,
+            0b00100000,
+            0b00100000,
+            0b00100000,
+            0b00100000,
+            0b00000000,
+            0b00000000,
+        ],
+        8 => [
+            0b11100000,
+            0b10100000,
+            0b11100000,
+            0b10100000,
+            0b10100000,
+            0b11100000,
+            0b00000000,
+            0b00000000,
+        ],
+        9 => [
+            0b11100000,
+            0b10100000,
+            0b11100000,
+            0b00100000,
+            0b00100000,
+            0b11100000,
+            0b00000000,
+            0b00000000,
+        ],
+        _=> [
+            0b11100000,
+            0b11100000,
+            0b11100000,
+            0b11100000,
+            0b11100000,
+            0b11100000,
+            0b00000000,
+            0b00000000,
+        ],
+    };
+    to_write
+}
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
@@ -30,14 +147,21 @@ async fn main(_spawner: Spawner) {
     let mut display = max7219::MAX7219::from_pins(4, data, cs, sck).unwrap();
 
     display.power_on().unwrap();
-    display.set_intensity(0, 0x1).unwrap();
+    display.set_intensity(0, 0x0).unwrap();
 
+    let mut counter = 0u8;
     loop {
-        //cs.set_low();
         led.set_low();
-        //display.write_str(0, b"pls help", 0b00100000).unwrap();
-        display.write_raw(0, &[0b100, 1, 0b10, 0, 0, 0, 0, 0]);
-        //cs.set_high();
+        // first value is first row, bits are cols
+        display.write_raw(0, &output_digit(counter));
+        display.write_raw(1, &output_digit(counter+1));
+        display.write_raw(3, &output_digit(counter+2));
+
+        counter += 1;
+        if (counter > 9) {
+            counter = 0;
+        }
+
         led.set_high();
 
         Timer::after_millis(1000).await;
