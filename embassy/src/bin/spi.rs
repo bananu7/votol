2026f925c4f5ccebbf96d3ff1bccc652c2aeb716
bananu7 <bananu7@o2.pl>
2025-02-07 01:10:41 +0100

    Made display init a util

--- embassy/src/bin/spi.rs
+++ embassy/src/bin/spi.rs
@@ -26,21 +26,12 @@ fn write_fullscreen_voltage<CONN: Connector>(voltage: u16, display: &mut max7219
     display.write_raw(3, &output_digit(frac));
 }
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
-    info!("Hello World!"); 
+fn setup_display(cs: Output<'static>, sck: Output<'static>, data: Output<'static>) -> max7219::MAX7219<max7219::connectors::PinConnector<Output<'static>, Output<'static>, Output<'static>>> {
     /*
     let mut spi_config = Config::default();
     spi_config.frequency = Hertz(1_000_000);
     let spi = Spi::new_blocking(p.SPI2, p.PB13, p.PB15, p.PB14, spi_config);
     */
-
-    let mut cs = Output::new(p.PB12, Level::High, Speed::VeryHigh);
-    let mut sck = Output::new(p.PB13, Level::High, Speed::VeryHigh);
-    let mut data = Output::new(p.PB15, Level::High, Speed::VeryHigh);
-
-    let mut led = Output::new(p.PC13, Level::High, Speed::Low);
 
     //let mut display = max7219::MAX7219::from_spi(4, spi).unwrap();
     let mut display = max7219::MAX7219::from_pins(4, data, cs, sck).unwrap();
@@ -48,17 +39,24 @@ async fn main(_spawner: Spawner) {
     display.power_on().unwrap();
     display.set_intensity(0, 0x0).unwrap();
 
-    let mut counter = 0u8;
+    display
+}
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+    info!("Hello World!"); 
+
+    let mut cs = Output::new(p.PB12, Level::High, Speed::VeryHigh);
+    let mut sck = Output::new(p.PB13, Level::High, Speed::VeryHigh);
+    let mut data = Output::new(p.PB15, Level::High, Speed::VeryHigh);
+
+    let mut display = setup_display(cs, sck, data);
+    let mut led = Output::new(p.PC13, Level::High, Speed::Low);
+
     loop {
         led.set_low();
-
-        write_fullscreen_voltage(529, &mut display);
-
-        counter += 1;
-        if counter > 9 {
-            counter = 0;
-        }
-
+        write_fullscreen_voltage(729, &mut display);
         led.set_high();
 
         Timer::after_millis(1000).await;
